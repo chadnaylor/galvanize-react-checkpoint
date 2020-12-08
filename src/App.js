8ba9fe598fa905df90192184a198f025890d89ec
Chad Naylor <chad.naylor@gmail.com>
2020-12-08 10:40:11 -0600

    fetch works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    emails: []
+  }
+
+  async componentDidMount() {
+    const response = await fetch('http://localhost:3001/emails')
+    const json = await response.json()
+    this.setState({ emails: json })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.emails.map((email) => {
+          return <p>{email.sender}: {email.message}</p>
+        })}
+      </div>
+    );
+  }
+
 }
 
 export default App;
